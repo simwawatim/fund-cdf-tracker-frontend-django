627118a2a82eee3e83c17c08f6381f3474ed4a4f
tim <simwawatimothy64@gmail.com>
2025-10-31 10:10:54 +0200

    added postgres db

--- cdf_backend/settings.py
+++ cdf_backend/settings.py
@@ -79,10 +79,19 @@ WSGI_APPLICATION = 'cdf_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'neondb_owner',
+        'PASSWORD': 'npg_VO6mBw9hDeEQ',
+        'HOST': 'ep-wispy-cloud-adfpc7lp-pooler.c-2.us-east-1.aws.neon.tech',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+            'channel_binding': 'require',
+        },
     }
 }
+
 
 
 # Password validation
